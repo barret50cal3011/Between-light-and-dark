1f0ffa9bdb1e4186f81d2babc204bd2b539157ca
B4RRET <barret50cal3011@gmail.com>
2022-10-31 23:27:35 -0600

    Created and link the sliders representing player stats

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    //gui variables
+    private Slider health_slider;
+    private Slider dark_mana_slider;
+    private Slider light_mana_slider;
+
     private int hp;
+    private int light_mana;
+    private int dark_mana;
 
     private void Awake() {
-        hp = 100;
+        
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        health_slider = GameObject.Find("Health_Slider").GetComponent<Slider>();
+        dark_mana_slider = GameObject.Find("Dark_Mana_Slider").GetComponent<Slider>();
+        light_mana_slider = GameObject.Find("Light_Mana_Slider").GetComponent<Slider>();
+
+        update_hp(100);
+        update_light_mana(0);
+        update_dark_mana(0);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void update_hp(int i_hp){
+        hp = i_hp;
+        health_slider.value = hp;
+    }
+
+    private void update_light_mana(int i_light_mana){
+        light_mana = i_light_mana;
+        light_mana_slider.value = light_mana;
+    }
+
+    private void update_dark_mana(int i_dark_mana){
+        dark_mana = i_dark_mana;
+        dark_mana_slider.value = dark_mana;
     }
 }
